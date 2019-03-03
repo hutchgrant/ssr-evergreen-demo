71af41d32c643192dbf2417caed4f8c480179ca4
HutchGrant <h.g.utchinson@gmail.com>
2019-03-03 05:55:45 -0500

    updating to ignore builds

--- buildClient/static/js/b2485acda8712d1a3960.js
+++
@@ -1,9 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{29:function(t,n,e){var o=e(30);t.exports="string"==typeof o?o:o.toString()},30:function(t,n,e){(t.exports=e(13)(!1)).push([t.i,":host p{color:#3c6919;font-size:1.75rem;margin:5px}",""])},31:function(t,n,e){"use strict";e.r(n);var o=e(0),r=e(29),c=e.n(r);let p=function(t){var n=t.getPrototypeOf||function(t){return t.__proto__},e=t.setPrototypeOf||function(t,n){return t.__proto__=n,t},o="object"==typeof Reflect?Reflect.construct:function(t,n,o){var r,c=[null];return c.push.apply(c,n),r=t.bind.apply(t,c),e(new r,o.prototype)};return function(t){var r=n(t);return e(t,e(function(){return o(r,arguments,n(this).constructor)},r))}}(Object)(class extends o.a{render(){return o["c"]`
-      <style>
-        ${c.a}
-      </style>
-
-      <div>
-        <p>Docs page!</p>
-      </div>
-    `}});customElements.define("docs-page",p)}}]);
